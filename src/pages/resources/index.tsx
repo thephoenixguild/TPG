10d922cdc0ce06ddfe76fd76661f88e668ced390
Manikandan <mani22be@gmail.com>
2023-08-31 13:51:28 +0530

    added env

--- src/pages/resources/index.tsx
+++ src/pages/resources/index.tsx
@@ -29,7 +29,7 @@ export default Resources
 
 export async function getServerSideProps() {
 
-    const res = await fetch(`http://localhost:3000/api/notion`)
+    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/notion`)
     const data = await res.json()
 
     return { props: { data } }
